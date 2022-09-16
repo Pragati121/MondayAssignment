0b2c7f6531668a5333bce6448191100ab5de7517
Pragati121 <Pragati@infostride.com>
2022-09-16 16:03:44 +0530

    Computer option

--- src/test/java/pages/DashboardPage.java
+++ src/test/java/pages/DashboardPage.java
@@ -1,15 +1,20 @@
 package pages;
 
-import net.jodah.failsafe.internal.util.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 
 
 import java.io.IOException;
 
 public class DashboardPage {
     WebDriver driver;
-    By HomePageVerify = By.xpath("//a[contains(@title,'Home')]");
+    By ComputerHyperlink = By.xpath("//a[text()='Computers ']");
+    By Notebook          = By.xpath("//a[text()=' Notebooks ']");
+    By Cart              = By.xpath("//button[contains(@class,'button-2')]");
+    By Cart2             = By.xpath("//button[text()=\"Add to cart\"][1]");
+    By Shoppingcart      =  By.xpath("//a[@class='ico-cart']");
+    By Checkout          = By.xpath("//button[@name='checkout']");
     public DashboardPage(WebDriver driver){
         this.driver = driver;
     }
@@ -17,9 +22,13 @@ public class DashboardPage {
     public void DashboardUser() throws InterruptedException
     {
          Thread.sleep(2000);
-        driver.findElement(HomePageVerify).click();
-        String actual = driver.findElement(HomePageVerify).getText();
-        Assert.isTrue(actual.equals("Dashboard"), "Expected result does not match with actual result");
+         driver.findElement(ComputerHyperlink).click();
+         driver.findElement(Notebook).click();
+         driver.findElement(Cart).click();
+        driver.findElement(Cart2).click();
+        driver.findElement(Shoppingcart).click();
+        driver.findElement(Checkout).click();
+
     }
 
 }
